884647face301c611eeb2672a07298ade7fe5765
shayma <shaymamukaram@gmail.com>
2018-09-13 16:58:59 +0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-  $("#change").click(function() {
-    $("aside").toggleClass(function() {});
+  $("#num").click(function() {});
+
+  $("#clear").click(function() {
+    $("#input").val("");
   });
 });
+
+$("#operation1").val("1");
+$("#operation2").val("2");
+$("#operation3").val("3");
+$("#operation4").val("4");
+$("#operation5").val("5");
+$("#operation6").val("6");
+$("#operation7").val("7");
+$("#operation8").val("8");
+$("#operation9").val("9");
+$("#operation0").val("0");
+$("#operation+").val("+");
+$("#operation-").val("-");
+$("#operation=").val("=");
+$("#operation/").val("/");
+$("#operation%").val("%");
